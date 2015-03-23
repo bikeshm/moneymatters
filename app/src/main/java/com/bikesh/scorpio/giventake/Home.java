89f9124d09ce0c5b903e2890db6bdcd33f4eac1a
bikesh <bikeshm@gmail.com>
2015-03-24 01:46:56 +0530

    implemented Navigation Drawerv

--- app/src/main/java/com/bikesh/scorpio/giventake/Home.java
+++ app/src/main/java/com/bikesh/scorpio/giventake/Home.java
@@ -1,13 +1,25 @@
 package com.bikesh.scorpio.giventake;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 
-public class Home extends ActionBarActivity {
+public class Home extends ActionBarActivity implements AdapterView.OnItemClickListener {
+
+    private DrawerLayout drawerLayout;
+    private ListView listView;
+    private String[] mItems;
+    private ActionBarDrawerToggle drawerListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +29,48 @@ public class Home extends ActionBarActivity {
         // Set a toolbar to replace the action bar.
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+
+        drawerLayout=(DrawerLayout) findViewById(R.id.drawerLayout);
+        listView=(ListView) findViewById(R.id.drawerList);
+
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        // ActionBarDrawerToggle ties together the the proper interactions
+        // between the sliding drawer and the action bar app icon
+        drawerListener = new ActionBarDrawerToggle(
+                this,                  /* host Activity */
+                drawerLayout,         /* DrawerLayout object */
+                toolbar,
+                R.string.open_navigation_drawer,  /* "open drawer" description for accessibility */
+                R.string.closing_navigation_drawer  /* "close drawer" description for accessibility */
+        ) {
+            public void onDrawerClosed(View view) {
+                //getSupportActionBar().setTitle(mTitle);
+                //supportInvalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+                Toast.makeText(Home.this, "Draweer closed", Toast.LENGTH_SHORT);
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                //getSupportActionBar().setTitle(mDrawerTitle);
+                // supportInvalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+                Toast.makeText(Home.this, "Draweer opend", Toast.LENGTH_SHORT);
+            }
+        };
+        drawerLayout.setDrawerListener(drawerListener);
+
+        //getting array from the xml array
+        mItems = getResources().getStringArray(R.array.menuItems);
+
+        //android.R.layout.simple_list_item_1 => it is coming with android stk by default
+        listView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mItems));
+
+        //adding click listener
+        listView.setOnItemClickListener(this);
+
 
     }
 
@@ -42,4 +96,32 @@ public class Home extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+
+
+
+
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+        Toast.makeText(this, mItems[position] + "Clicked", Toast.LENGTH_LONG).show();
+        listView.setItemChecked(position, true);
+
+        getSupportActionBar().setTitle(  mItems[position] );
+    }
+
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerListener.syncState();
+    }
+
+
+
+
+
+
 }
