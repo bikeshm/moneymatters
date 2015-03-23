1e35d754fe335b7b2fed1d5c0ba78d1de04cb268
bikesh <bikeshm@gmail.com>
2015-03-24 01:23:38 +0530

    implemented cusome tool bar

--- app/src/main/java/com/bikesh/scorpio/giventake/Home.java
+++ app/src/main/java/com/bikesh/scorpio/giventake/Home.java
@@ -2,6 +2,7 @@ package com.bikesh.scorpio.giventake;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -12,6 +13,11 @@ public class Home extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        // Set a toolbar to replace the action bar.
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
     }
 
 
