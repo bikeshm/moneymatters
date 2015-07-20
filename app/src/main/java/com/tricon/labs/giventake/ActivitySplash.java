bc923180d1b4be27e4caedc467d4ceae5845113f
bikesh <android1@myfriendsgroup.com>
2015-07-20 19:08:49 +0530

    Removed registration process from the beginning (planed to make this in group expense )

--- app/src/main/java/com/tricon/labs/giventake/ActivitySplash.java
+++ app/src/main/java/com/tricon/labs/giventake/ActivitySplash.java
@@ -3,18 +3,22 @@ package com.tricon.labs.giventake;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.EditText;
 
 import com.tricon.labs.giventake.database.DBHelper;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class ActivitySplash extends AppCompatActivity {
 
 
-    DBHelper myDb;
+    private DBHelper mDBHelper;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +30,7 @@ public class ActivitySplash extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
 
 
-        myDb = new DBHelper(this);
+        mDBHelper = new DBHelper(this);
 
         new Handler().postDelayed(new Runnable() {
 
@@ -37,60 +41,21 @@ public class ActivitySplash extends AppCompatActivity {
                 Intent i=new Intent(ActivitySplash.this, ActivityHome.class);
 
                 // if first time opening the app , need to register root user (me user)
-                if(myDb.getNumRowsUsertable()==0){
-                    //register user
-                    i = new Intent(ActivitySplash.this, ActivityRegisterUser.class);
-                    i.putExtra("fromActivity", "ActivitySplash");
+                if(mDBHelper.getNumRowsUsertable()==0){
+
+                    //registering root user
+                    Map<String, String> data = new HashMap<String, String>();
+                    data.put("_id", "1");
+                    data.put("name", "Me");
+                    mDBHelper.insertUser(data);
 
                 }
-
-
                 startActivity(i);
                 finish();
             }
         }, 3*1000); // wait for 3 seconds
 
-
-
-
-        /* Assinging the toolbar object ot the view
-        and setting the the Action bar to our toolbar
-         */
-
-
-        /*
-        Toolbar toolbar = (Toolbar) findViewById(R.id.tool_bar);
-        setSupportActionBar(toolbar);
-
-        GiveNTakeApplication AC = (GiveNTakeApplication)getApplicationContext();
-        View view = getWindow().getDecorView().findViewById(android.R.id.content);
-        AC.setupDrawer(view, ActivitySplash.this, getFragmentManager(), toolbar );
-        */
-
-
-        /*
-        if (toolbar != null) {
-            setSupportActionBar(toolbar);
-            getSupportActionBar().setTitle("sc");
-            //Set SubTitle
-           // toolbar.setSubtitle("Sub Title");
-            getSupportActionBar().setHomeButtonEnabled(true);
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        }*/
-
-
-
-
     } // onreate end
-
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        if (myDb != null) {
-            myDb.close();
-        }
-    }
 
 }
 
