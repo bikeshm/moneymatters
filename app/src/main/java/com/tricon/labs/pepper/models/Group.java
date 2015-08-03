810d2264402a83a4706212724c145fd8122b820b
bikesh <android1@myfriendsgroup.com>
2015-08-03 14:24:32 +0530

    committed group model

--- app/src/main/java/com/tricon/labs/pepper/models/Group.java
+++ app/src/main/java/com/tricon/labs/pepper/models/Group.java
@@ -1,10 +1,17 @@
 package com.tricon.labs.pepper.models;
 
-public class Group {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Group implements Parcelable {
     public int id;
+    public int onlineid;
+    public int isonline;
+    public int owner;
     public String name;
     public Float totalAmount;
     public int membersCount;
+    public int ismonthlytask;
     public Float balanceAmount;
     public Float amountPerHead;
     public Float amountSpentByMe;
@@ -14,4 +21,58 @@ public class Group {
 
     public static final int STATUS_GIVE = 0;
     public static final int STATUS_GET = 1;
+
+    protected Group(Parcel in) {
+        id = in.readInt();
+        onlineid = in.readInt();
+        isonline = in.readInt();
+        owner = in.readInt();
+        name = in.readString();
+        totalAmount = in.readFloat();
+        membersCount = in.readInt();
+        ismonthlytask = in.readInt();
+        balanceAmount = in.readFloat();
+        amountPerHead = in.readFloat();
+        amountSpentByMe = in.readFloat();
+        amountSpentByMeCurrentMonth = in.readFloat();
+        status = in.readInt();
+
+    }
+
+    public static final Creator<Group> CREATOR = new Creator<Group>() {
+        @Override
+        public Group createFromParcel(Parcel in) {
+            return new Group(in);
+        }
+
+        @Override
+        public Group[] newArray(int size) {
+            return new Group[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+
+        dest.writeInt(id);
+        dest.writeInt(onlineid);
+        dest.writeInt(isonline);
+        dest.writeInt(owner);
+        dest.writeString(name);
+        dest.writeFloat(totalAmount);
+        dest.writeInt(membersCount);
+        dest.writeInt(ismonthlytask);
+        dest.writeFloat(balanceAmount);
+        dest.writeFloat(amountPerHead);
+        dest.writeFloat(amountSpentByMe);
+        dest.writeFloat(amountSpentByMeCurrentMonth);
+        dest.writeInt(status);
+
+
+    }
 }
