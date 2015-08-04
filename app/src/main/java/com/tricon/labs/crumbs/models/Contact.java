2c896b30a3022976793cfbd9466c6a30845c81a6
bikesh <android1@myfriendsgroup.com>
2015-08-04 15:52:54 +0530

    added group expense entry model

--- app/src/main/java/com/tricon/labs/crumbs/models/Contact.java
+++ app/src/main/java/com/tricon/labs/crumbs/models/Contact.java
@@ -1,6 +1,9 @@
 package com.tricon.labs.crumbs.models;
 
-public class Contact {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Contact implements Parcelable {
     public int id;
     public String name;
     public String phone;
@@ -11,6 +14,24 @@ public class Contact {
         this.phone = phone;
     }
 
+    protected Contact(Parcel in) {
+        id = in.readInt();
+        name = in.readString();
+        phone = in.readString();
+    }
+
+    public static final Creator<Contact> CREATOR = new Creator<Contact>() {
+        @Override
+        public Contact createFromParcel(Parcel in) {
+            return new Contact(in);
+        }
+
+        @Override
+        public Contact[] newArray(int size) {
+            return new Contact[size];
+        }
+    };
+
     @Override
     public boolean equals(Object contact) {
         return contact instanceof Contact && this.phone.equals(((Contact) contact).phone);
@@ -19,5 +40,17 @@ public class Contact {
     @Override
     public int hashCode() {
         return this.phone.hashCode();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(id);
+        dest.writeString(name);
+        dest.writeString(phone);
     }
 }
